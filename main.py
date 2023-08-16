35cb2f390ee36b038c99bea45a707415aa42b02b
MarcelFeo <marcelfeo29@gmail.com>
2023-08-16 15:11:09 -0300

    adicionando função para gerar public key

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ def generate_private_key():
     # gerando a chave privada "aleatoriamente" entre um intervalo [1, n]
     return ecdsa.util.randrange(1, n)
 
-
+def generate_public_key(private_key):
+    # gerando a chave pública
+    return private_key * generator
