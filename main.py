a74c136e5e88a4803ff16b5a4a36b9d1a234139b
MarcelFeo <marcelfeo29@gmail.com>
2023-08-16 15:13:30 -0300

    adicionando função para gerar bitcoin address

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def generate_private_key():
 def generate_public_key(private_key):
     # gerando a chave pública
     return private_key * generator
+
+def generate_bitcoin_address(public_key):
+    public_key_bytes = public_key.to_string()
+    sha256_hash = sha256(public_key_bytes).digest()
+    ripemd160_hash = hashlib.new("ripemd160", sha256_hash).digest()
+    address_bytes = b"\x00" + ripemd160_hash
+    checksum = sha256(sha256(address_bytes).digest()).digest()[:4]
+    bitcoin_address = base58.b58encode(address_bytes + checksum).decode("utf-8")
+    return bitcoin_address
