10fc3dcb6a52bc35398828b18076d8197e2ba5dd
MarcelFeo <marcelfeo29@gmail.com>
2023-08-16 15:10:08 -0300

    adicionando função para gerar private key

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from hashlib import sha256
 import ecdsa
 import base58
 import requests
+
+def generate_private_key():
+    # gerando a chave privada "aleatoriamente" entre um intervalo [1, n]
+    return ecdsa.util.randrange(1, n)
+
+
